af5eb9707164f4fecf80dbd1f49a4dc502a62d2d
Enyell Duarte <engell.sof.vall@gmail.com>
2023-05-11 15:23:46 -0400

    Primer commit del curso de scope y closure

---
+++ src/scope/block.js
@@ -0,0 +1,7 @@
+function fruit() {
+    if (true) {
+        var fruit1 = 'Apple';
+        let fruit2 = 'Kiwi';
+        const fruit = 'Banana';
+    }
+}
---
+++ src/scope/function.js
@@ -0,0 +1,12 @@
+function saludo() {
+    let userName = 'Pepito';
+    console.log(userName);
+
+    if (userName === 'Pepito') {
+        console.log(`Hello ${userName}!`);
+    }
+
+}
+
+saludo();
+console.log(userName);
